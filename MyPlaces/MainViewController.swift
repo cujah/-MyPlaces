9b46e82d435973bfb7608046b8ffb35f5eac0673
ILYA <cujah@mail.ru>
2021-10-06 14:51:22 +0300

    Initial

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,66 @@
+//
+//  MainTableViewController.swift
+//  MyPlaces
+//
+//  Created by Илья on 06.10.2021.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let placeNames = ["BetankurSkatePlaza",
+                      "СпотПодМостом",
+                      "ЯмаДыбенко",
+                      "Смена",
+                      "Жесть",
+                      "StreetSportAcademy",
+                      "Бугры",
+                      "Черная речка",
+                      "Передовиков",
+                      "Парк 300-Летия",
+                      "Сестроретск рампа",
+                      "VseVPark",
+                      "Ломоносов",
+                      "Молодежное",
+                      "Гатчина"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return placeNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = placeNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: placeNames[indexPath.row])
+        
+        return cell
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
