0eba39adf49700237fff649dbdac215aa45e79f2
ILYA <cujah@mail.ru>
2021-10-14 13:06:47 +0300

    Work with Annotations

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -13,9 +13,11 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     var place: Place!
+    let annotationIdentifier = "annotationIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
           setupPlacemark()
     }
 
@@ -28,8 +30,8 @@ class MapViewController: UIViewController {
     private func setupPlacemark(){
         guard let location = place.location else { return }
         
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(location) { placemarks, error in
+        let geocoder = CLGeocoder()                                     // преобразует адрес в координаты
+        geocoder.geocodeAddressString(location) { placemarks, error in  // geocodeAddressString координаты из string
             if let error = error {
                 print (error)
                 return
@@ -51,9 +53,38 @@ class MapViewController: UIViewController {
             
             self.mapView.selectAnnotation(annotation, animated: true)   // выделение аннотации
             
-            
-            
         }
+
+    }
+    
+}
+
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard !(annotation is MKUserLocation) else { return nil }  // проверка не является ли аннотация текущей                                                                 позицией пользователя
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView             // приводим к типу MKPinAnnotationView тк у него есть уже маркер (булавка)
+        
+        
+        // если на карте не окажется ни одного представления с аннотацией для переиспользования, то инициализируем новый объект соответствующими значениями:
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true           // отображаем аннотацию в виде баннера
+        }
+        
+        // отображаем фото места на баннере
+        if let imageData = place.imageData {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView                       // отображаем фото справа
+        }
+        
+        return annotationView
         
     }
     
